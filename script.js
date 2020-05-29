c154d46177cac7461f0a9417d3948f86e01b03a0
therset <rset.agency@gmail.com>
2020-05-29 08:03:26 +0100

    changes

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ function staticLoadPlaces() {
 
 var models = [
     {
-        url: './assets/magnemite/scene.gltf',
+        url: './assets/WhipperNude/WhipperNude.gltf',
         scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
+        info: 'lady',
         rotation: '0 180 0',
     },
     {
